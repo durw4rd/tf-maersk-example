c39472005d9ed07425953fdcd2cd3ae7d4fa05c1
Michal Fasanek <misa.fasa@gmail.com>
2025-04-14 15:55:50 +0200

    add example of using role attribtues

--- main.tf
+++ main.tf
@@ -7,15 +7,37 @@ resource "launchdarkly_custom_role" "tf_example_role" {
   key         = "tf-example-role"
   name        = "Example Terraform role"
   description = "Role description"
-  base_permissions  = "reader"
+  base_permissions = "no_access"
 
   policy_statements {
     resources = [
-      "application/*"
+      "proj/$${roleAttribute/project-key}"
     ]
     actions = [
-      "*"
+      "viewProject"
     ]
     effect = "allow"
+  }
+}
+
+resource "launchdarkly_team" "tf_example_team" {
+  for_each = toset(var.squad_names)
+
+  key         = "${each.key}-team"
+  name        = "${each.key} team"
+  description = "Team for ${each.key}"
+
+  custom_role_keys = [
+    "tf-example-role"
+  ]
+
+  role_attributes {
+    key = "project-key"
+    values = [each.key]
+  }
+
+  # ignore changes to team membership to avoid overwriting IDP assignments
+  lifecycle {
+    ignore_changes = [member_ids]
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,5 +2,10 @@ variable "LD_API_KEY" {
   type        = string
   description = "LaunchDarkly API access token"
   sensitive   = true
-  default     = "<PROVIDE-YOUR-API-KEY>"
+  default     = "<add your API access token>"
+}
+
+variable "squad_names" {
+  type    = list(string)
+  default = ["reds", "blues", "greens"] 
 }
